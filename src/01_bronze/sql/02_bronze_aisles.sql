395c1a854f9e5a8f75fccb489670c1398b7260cc
Nadine <canquinf@gmail.com>
2026-09-01 21:38:12 +0800

    Update 02_bronze_aisles.sql

--- src/01_bronze/sql/02_bronze_aisles.sql
+++ src/01_bronze/sql/02_bronze_aisles.sql
@@ -1,2 +1,19 @@
 -- saved-query name: 02_bronze_aisles
 -- paste the approved Databricks SQL for this task below
+-- replaces the bronze table using the approved explicit source schema
+
+CREATE OR REPLACE TABLE workspace.instacart_bronze.aisles
+USING DELTA
+COMMENT 'bronze copy of the instacart aisles csv'
+AS
+SELECT
+  aisle_id,
+  aisle,
+  _rescued_data
+FROM read_files(
+  '/Volumes/workspace/default/ftw-b12-de-r2/shared/week06/instacart_csv/aisles.csv',
+  format => 'csv',
+  header => true,
+  schema => 'aisle_id INT, aisle STRING',
+  rescuedDataColumn => '_rescued_data'
+);
