aa43af16f9cfd1fb42f007e4d48384bac646486f
catherine.balane@ftwfoundation.org <catherine.balane@ftwfoundation.org>
2026-09-01 16:38:48 +0000

    bronze layer

--- src/01_bronze/sql/06_bronze_order_products_prior.sql
+++ src/01_bronze/sql/06_bronze_order_products_prior.sql
@@ -1,2 +1,14 @@
 -- saved-query name: 06_bronze_order_products_prior
 -- paste the approved Databricks SQL for this task below
+-- TABLE: order_products__prior
+CREATE OR REPLACE TABLE order_products__prior AS
+SELECT * FROM read_files(
+  '/Volumes/workspace/default/ftw_b12_de/shared/week06/instacart_csv/order_products__prior.csv',
+  format => 'csv', header => true,
+  schema => 'order_id INT,product_id INT,add_to_cart_order INT,reordered INT'
+);
+
+DESCRIBE TABLE order_products__prior;
+
+SELECT * 
+FROM order_products__prior;
