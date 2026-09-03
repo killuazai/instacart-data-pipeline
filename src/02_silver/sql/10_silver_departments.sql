d121ba1a6230a4c50fb153c573fdc4dcdd4c5e4d
ina.magno@ftwfoundation.org <ina.magno@ftwfoundation.org>
2026-09-03 13:17:05 +0000

    Deleted redundant queries.

--- src/02_silver/sql/10_silver_departments.sql
+++ src/02_silver/sql/10_silver_departments.sql
@@ -11,21 +11,3 @@ FROM instacart_bronze.departments
 WHERE department_id IS NOT NULL;
 
 DESCRIBE TABLE departments_clean;
--- saved-query name: 10_silver_departments
--- paste the approved Databricks SQL for this task below
-
-CREATE OR REPLACE TABLE silver_departments AS
-
-SELECT DISTINCT
-    CAST(department_id AS INT) AS department_id,
-    TRIM(LOWER(department)) AS department
-
-FROM bronze_departments
-
-WHERE department_id IS NOT NULL
-  AND department IS NOT NULL
-  AND TRIM(department) != '';
-
-  SELECT *
-FROM silver_departments
-ORDER BY department_id;
